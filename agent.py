ded9b98470b613198bf5f35efd653b7f436a6821
Saphal Kafle <saphalkafle77@gmail.com>
2026-07-06 19:45:07 +0545

    defining agent

--- agent.py
+++ agent.py
@@ -9,19 +9,23 @@ elif torch.cuda.is_available():
 else
     device = "cpu"
 
-     
-env = gym.make("FlappyBird-v0",render_mode = "human")
 
-state,_ = env.reset()
-while True:
-    #Next action:
-    #(feed the observation to your agent here)
-    action = env.action_space.sample()
+def run(self, is_training = True, render = False)
+    env = gym.make("FlappyBird-v0",render_mode = "human" if render else None)
+    
+    num_states = env.observation_space.shape[0] #input dimension
+    num_actions = env.action_space.n #output dimension
+    policy_dqn = DQN(num_states,num_actions).to(device)
+    state,_ = env.reset()
+    while True:
+        #Next action:
+        #(feed the observation to your agent here)
+        action = env.action_space.sample()
 
-    #Processing: terminate => done
-    next_state,reward,terminated,_ = env.step(action)
+        #Processing: terminate => done
+        next_state,reward,terminated,_ = env.step(action)
 
-    #checking if the player is still alive
-    if terminated:
-        break
-env.close
+        #checking if the player is still alive
+        if terminated:
+            break
+    env.close
