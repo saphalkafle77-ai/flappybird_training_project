d2890738a3698d7e3d46111e548e7e201adb7ffe
Saphal Kafle <saphalkafle77@gmail.com>
2026-07-07 18:46:45 +0545

    optimize function

--- agent.py
+++ agent.py
@@ -12,7 +12,7 @@ if torch.backendmps.is_available():
     device = "mps"
 elif torch.cuda.is_available():
     device = "cuda"
-else
+else:
     device = "cpu"
 
 
@@ -44,7 +44,7 @@ class Agent:
         self.optimizer = None
 
 
-    def run(self, is_training = True, render = False)
+    def run(self, is_training = True, render = False):
         env = gym.make("FlappyBird-v0",render_mode = "human" if render else None)
         
         num_states = env.observation_space.shape[0] #input dimension
@@ -56,6 +56,15 @@ class Agent:
         if is_training:
             memory = ReplayMemory(self.replay_memory_size)
             epsilon = self.epsilon_init
+
+            target_dqn = DQN(num_states,num_actions).to(device)
+
+            #copy wt and bias vals from policy ==> target
+            target_dqn.load_state_dict(policy_dqn.state_dict())
+
+            steps = 0
+
+            self.optimizer = optim.Adam(policy_dqn.parameters(),lr=self.alpha)
 
         for episode in itertools.count():
             state,_ = env.reset()
@@ -84,12 +93,53 @@ class Agent:
 
                 if is_training:
                     memory.append((state,action,new_state,reward,terminated))
+                    steps += 1
                 
                 state = new_state
                 episode_rewards += reward
             
             print(f"episode = {episode+1} with total reward = {episode_rewards} & epsilon{epsilon}")
 
-            #epsilon decay
-            epsilon -  max(epsilon * self.epsilon_decay , self.epsilon.min)
+
+            if is_training:
+                #epsilon decay
+                epsilon =  max(epsilon * self.epsilon_decay , self.epsilon.min)
+
+            if is_training and len(memory) > self.mini_batch_size: #sync networks
+                #get sample 
+                mini_batch = memory.sample(self.mini_batch_size)
+
+                optimize(mini_batch,policy_dqn,target_dqn)
+
+                #sync the network
+                if steps > self.netword_sync_rate:
+                    target_dqn.load_state_dict(policy_dqn.state_dict())
+                    steps = 0
             #env.close
+    def optimize(self,mini_batch,policy_dqn,target_dqn):
+        #get experience ==> in batch for fast training
+        state,action,next_state,reward,terminated = zip(*mini_batch)
+
+        state = torch.stack(state)
+        action = torch.stack(action)
+        next_state = torch.stack(next_state)
+        rewards = torch.stack(rewards)
+        terminated = torch.tensor(terminated).float().to(device)
+
+
+        
+
+    
+        with torch.no_grad():
+            target_q = reward + (1-terminated) * self.gamma * target_dqn(next_state).max(dim=1)[0]
+
+        current_q = policy_dqn(state).gather(dim=1,index = action.unsqueeze(dim=1)).squeeze()
+
+
+            #loss
+        loss = self.loss_fn(current_q,target_q)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
